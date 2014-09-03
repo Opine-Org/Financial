2b45453e8f3cd2e67d06653609845fd92820a2b7
rmahoney-bsd <rmahoney@bluestatedigital.com>
2014-09-03 10:44:20 -0400

    minor changes

--- tests/FinancialTest.php
+++ tests/FinancialTest.php
@@ -7,7 +7,7 @@ class FinancialTest extends \PHPUnit_Framework_TestCase {
 
     public function setup () {
         date_default_timezone_set('UTC');
-        $root = getcwd();
+        $root = __DIR__;
         $container = new Container($root, $root . '/container.yml');
         $this->financial = $container->financial;
         $this->db = $container->db;
